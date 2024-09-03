cb745c8b52ecf83981a9f17c2864c83cbc8dc571
Camargo11-max <cristiancamargo001@gmail.com>
2024-09-02 22:36:20 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//console.log('propiedades del documento',document.querySelector('p') )
+//console.log('propiedades del documento',document)
+//console.log('propiedades del documento',document.querySelector('.parrafo') )
+//console.log('propiedades del documento',document.querySelector('#parrafo') )
+
+/* const selectparrafo = document.querySelector('h1');
+
+selectparrafo.addEventListener("click", getNombre);
+
+function getNombre () {
+    console.log('Ingreso a la función getNombre');
+}
+
+getNombre(); */
+
+
